3900a0cbde3aebf717b742bf85cf4a163c2a1888
yuneszadi <yuneszadi@gmail.com>
2020-10-21 21:38:15 +0300

    replace inputs with plain values for rate in person card

--- app/overrides/update_people.rb
+++ app/overrides/update_people.rb
@@ -2,6 +2,6 @@ if Redmine::Plugin.installed?(:redmine_people)
   Deface::Override.new :virtual_path  => "people/show",
                        :name          => "add_rate_form_to_people_settings",
                        :insert_after  => 'div.details',
-                       :text          => "<div id='issue_tree'><%= render 'rate_ratings/user_rate'%></div>"
+                       :text          => "<div id='issue_tree'><%= render 'rate_ratings/rate_ratings_people'%></div>"
 
 end
--- app/overrides/update_people_edit.rb
+++ app/overrides/update_people_edit.rb
@@ -1,6 +1,6 @@
 if Redmine::Plugin.installed?(:redmine_people)
   Deface::Override.new :virtual_path  => "people/_form",
-                       :name          => "add_rate_form_to_people_settings_1",
+                       :name          => "add_rate_form_to_people_edit_settings",
                        :insert_after  => 'div#person_data',
                        :text          => "<div id='issue_tree'><%= render 'rate_ratings/user_rate'%></div>"
 
