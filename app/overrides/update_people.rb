9d18565cebac22a96278fd22e0d933107349ef40
yuneszadi <yuneszadi@gmail.com>
2020-11-06 19:50:18 +0200

    add rates sidebar for people

--- app/overrides/update_people.rb
+++ app/overrides/update_people.rb
@@ -1,7 +1,7 @@
 if Redmine::Plugin.installed?(:redmine_people)
-  Deface::Override.new :virtual_path  => "people/show",
+  Deface::Override.new :virtual_path  => "people/_attributes",
                        :name          => "add_rate_form_to_people_settings",
-                       :insert_after  => 'div.details',
+                       :insert_after  => 'div#attributes',
                        :text          => "<div id='issue_tree'><%= render 'rate_ratings/rate_ratings_people'%></div>"
 
 end
